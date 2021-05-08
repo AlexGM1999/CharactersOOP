de5ea7cf51304e15999ce03fbd255b4b666ccd5c
Alex <alexgm19991@gmail.com>
2021-05-08 19:13:43 +0300

    connecting to DB using Entity Framework (code first)

---
+++ CharactersOOP/Models/Character.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CharactersOOP
+{
+    public class Character
+    {
+        [Key]
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+
+        public Character(int health, int attack, string name)
+        {
+            Health = health;
+            Attack = attack;
+            Name = name;
+        }
+
+        public void Damage(int damage)
+        {
+            Health -= damage;
+        }
+
+
+    }
+}
---
+++ CharactersOOP/Models/CharacterContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CharactersOOP.Models
+{
+    public class CharacterContext: DbContext
+    {
+        public DbSet<Character> Characters { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=CharactersDB;Trusted_Connection=True;");
+        }
+    }
+}
